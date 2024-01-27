ae6f46f5523e8cd0eee27aed7bb2bbbeff7edbb6
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-01-27 11:56:31 -0500

    Add api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = "http://omdbapi.com?apikey=a87e2211";
 
 const App = () => {
+
+    
+
+useEffect(() => {
+
+},[]);
+
     return (
         <h1>App</h1>
     );
