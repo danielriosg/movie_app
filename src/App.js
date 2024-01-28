b85d85268de3ff45568760226a6ba0956acadbf5
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-01-28 12:11:53 -0500

    Add searchMovies function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ const API_URL = "http://omdbapi.com?apikey=a87e2211";
 
 const App = () => {
 
-    
+    const searchMovies = async (title) => {
+        const response = await fetch (`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
 
 useEffect(() => {
-
+searchMovies('Spiderman');
 },[]);
 
     return (
