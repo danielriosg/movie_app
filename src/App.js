9a88c380e42da42eb67677666dfdb914006b8886
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-01-28 12:56:18 -0500

    Add search input and icon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import { useEffect } from "react";
 
-import './App.css';
-import SearchIcon from './search.svg';
+import "./App.css";
+import SearchIcon from "./search.svg";
 const API_URL = "http://omdbapi.com?apikey=a87e2211";
 
-
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
 
-    const searchMovies = async (title) => {
-        const response = await fetch (`${API_URL}&s=${title}`);
-        const data = await response.json();
-        console.log(data.Search);
-    }
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
-useEffect(() => {
-searchMovies('Spiderman');
-},[]);
-
-    return (
-        <h1>App</h1>
-    );
-}
+  return (
+    <div className='app'>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" 
+        onClick={() =>{}}/>
+      </div>
+    </div>
+  );
+};
 
 export default App;
